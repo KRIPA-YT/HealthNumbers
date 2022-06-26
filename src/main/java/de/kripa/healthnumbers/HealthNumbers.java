df19d7f8805bcc6ee6c81eefeb39863762deed70
KRIPA-YT <hbqacc@gmail.com>
2022-06-26 16:36:06 +0200

    1.19 Conversion Try 2.1

--- src/main/java/de/kripa/healthnumbers/HealthNumbers.java
+++ src/main/java/de/kripa/healthnumbers/HealthNumbers.java
@@ -1,13 +1,5 @@
 package de.kripa.healthnumbers;
-
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import de.kripa.healthnumbers.mixins.InGameHudMixin;
-import lombok.Getter;
-import lombok.Setter;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
 
 public class HealthNumbers implements ClientModInitializer {
     public boolean modOnline = true;
