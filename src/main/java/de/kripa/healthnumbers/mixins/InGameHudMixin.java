6dd00e4a0ff6671ecb5098d214742a51be839c2c
KRIPA-YT <hbqacc@gmail.com>
2022-06-26 09:15:09 +0200

    OCD 4

--- src/main/java/de/kripa/healthnumbers/mixins/InGameHudMixin.java
+++ src/main/java/de/kripa/healthnumbers/mixins/InGameHudMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +15,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
@@ -33,10 +37,19 @@ public abstract class InGameHudMixin extends DrawableHelper {
     private void renderStatusBars(MatrixStack matrices, CallbackInfo info) {
         if(HealthNumbers.getMod().modOnline) {
             MinecraftClient client = MinecraftClient.getInstance();
-            client.textRenderer.drawWithShadow(matrices, (int) getCameraPlayer().getHealth() + "/" + (int) getCameraPlayer().getMaxHealth(), scaledWidth / 2 - 91, scaledHeight - 40, getCameraPlayer().getActiveStatusEffects().containsKey(StatusEffects.WITHER) ? 0x272727 : 0xdd2222);
+            int health = (int) getCameraPlayer().getHealth() + (int) Math.ceil(getCameraPlayer().getAbsorptionAmount());
+            int maxHealth = (int) getCameraPlayer().getMaxHealth();
             int food = getCameraPlayer().getHungerManager().getFoodLevel();
-            client.textRenderer.drawWithShadow(matrices, food + "/20", scaledWidth / 2 + 62 + (food < 10 ? client.textRenderer.getWidth("5") : 0),  scaledHeight - 40, 0xa76a40);
             int air = (getCameraPlayer().getAir() + 20) / 16;
+            Map<StatusEffect, StatusEffectInstance> effects = getCameraPlayer().getActiveStatusEffects();
+            int color = 0xdd2222;
+            if(effects.containsKey(StatusEffects.WITHER)) {
+                color = 0x272727;
+            } else if(effects.containsKey(StatusEffects.ABSORPTION)) {
+                color = 0xd4ac3c;
+            }
+            client.textRenderer.drawWithShadow(matrices, health + "/" + maxHealth, scaledWidth / 2 - 91, scaledHeight - 40, color);
+            client.textRenderer.drawWithShadow(matrices, food + "/20", scaledWidth / 2 + 62 + (food < 10 ? client.textRenderer.getWidth("5") : 0),  scaledHeight - 40, 0xa76a40);
             if(air == 20) {
                 // Saturation
                 int sat = (int) getCameraPlayer().getHungerManager().getSaturationLevel();
