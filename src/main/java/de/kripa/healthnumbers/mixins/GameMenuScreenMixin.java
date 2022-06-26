837679ec3995defcbbdaeef09efc0fd3344f9330
KRIPA-YT <hbqacc@gmail.com>
2022-06-26 16:34:16 +0200

    1.19 Conversion Try 2

--- src/main/java/de/kripa/healthnumbers/mixins/GameMenuScreenMixin.java
+++ src/main/java/de/kripa/healthnumbers/mixins/GameMenuScreenMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +18,7 @@ public class GameMenuScreenMixin extends Screen {
 
     @Inject(at= @At("HEAD"), method = "initWidgets")
     private void initWidgets(CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(10, 10, 120, 20, new TranslatableText(HealthNumbers.getMod().modOnline ? "healthnumbers.options.disable" : "healthnumbers.options.enable"), (button) -> {
+        this.addDrawableChild(new ButtonWidget(10, 10, 120, 20, Text.translatable(HealthNumbers.getMod().modOnline ? "healthnumbers.options.disable" : "healthnumbers.options.enable"), (button) -> {
             HealthNumbers.getMod().modOnline = !HealthNumbers.getMod().modOnline;
             client.setScreen(this);
         }));
